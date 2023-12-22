81b1ac732c571a3e8ad65e2cbc4afdb6653d3d22
berlinli2 <152087604+berlinli2@users.noreply.github.com>
2023-12-22 19:42:09 +0300

    anasayfa.py

--- anasayfa.py
+++ anasayfa.py
@@ -14,16 +14,16 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
-     ______                         _     
-    |  ____|                       | |    
-    | |__   _ __   ___  _   _  __ _| |__  
-    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
-    | |____| | | | (_) | |_| | (_| | | | |
-    |______|_| |_|\___/ \__,_|\__, |_| |_|
-                               __/ |      
-                              |___/      
+     ______                              
+    |  ____|                           
+    | |____|    
+    |  ____|  
+    | |____| 
+    |______| ombers
+                                     
+                                    
     
-    Sms: {}           {}by {}@tingirifistik\n  
+    Sms: {}           {}by {}@berlinli2\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n\n 2- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
